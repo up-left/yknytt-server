8180c4e1f955cebcbfd5825881ae3cae0995ce4e
up-left <left.right.forward@gmail.com>
2020-12-25 22:13:20 +0700

    Updated level page size; updated worlds.csv (just in case)

--- yknytt_server/settings.py
+++ yknytt_server/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'yknytt_server.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 10
 }
 
 # Database
