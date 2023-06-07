ebda2163b7dd298223526b08bf12a494dac1d237
up-left <left.right.forward@gmail.com>
2023-06-07 23:19:55 +0700

    Change admin submodule

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path(r'levels/', views.LevelList.as_view(), name='levels'),
     path(r'rate/', views.rate, name='rate'),
     path(r'rating/', views.RatingRetrieve.as_view(), name='rating'),
+
+    re_path(r"^admin/mcadmin/", include("mcadmin.urls")),
 ]
--- yknytt_server/settings.py
+++ yknytt_server/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_command_admin'
+    "mcadmin",
 ]
 
 MIDDLEWARE = [
