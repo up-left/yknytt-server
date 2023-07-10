b221a6379c3850c39d9bab571196e88c06aa99b1
up-left <left.right.forward@gmail.com>
2023-07-10 22:20:57 +0700

    Update page size and sorting

--- yknytt_server/settings.py
+++ yknytt_server/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'yknytt_server.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 20
 }
 
 # Database
