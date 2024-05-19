e44020de547e043ecbc8c9db1e90a06b5c19f768
up-left <left.right.forward@gmail.com>
2024-05-19 17:32:37 +0700

    Add complete rate if all endings were achieved (fix 2)

--- main/views.py
+++ main/views.py
@@ -107,10 +107,15 @@ def rate(request):
 
         if action in (6, 9):
             if Rate.objects.filter(level=level, uid=uid, action=6).count() >= Cutscene.objects.filter(level=level, ending=True).count():
-                _, complete_created = Rate.objects.get_or_create({'name': level_name, 'author': level_author, 'platform': platform, 'ip': ip},
-                        level=level, uid=uid, action=41)
-                if complete_created:
-                    LevelRating.objects.filter(level=level).update(completions=F('completions') + 1)
+                prev_rate = Rate.objects.filter(level=level, uid=uid, action__gte=40, action__lte=46).order_by('-time').first()
+                if prev_rate:
+                    prev_rate_field = Rate.ACTION_DICT[prev_rate.action]
+                    LevelRating.objects.filter(level=level).update(**{prev_rate_field: F(prev_rate_field) - 1})
+                    prev_rate.delete()
+
+                Rate.objects.get_or_create({'name': level_name, 'author': level_author, 'platform': platform, 'ip': ip},
+                    level=level, uid=uid, action=41, cutscene=cutscene)
+                LevelRating.objects.filter(level=level).update(completions=F('completions') + 1)
 
     if score_action:
         scores = {r.uid: r.action for r in Rate.objects.filter(level=level, action__gte=20, action__lte=30).order_by('uid', 'time')}.values()
